0dac38d38ed3eb60a3f73e18dc7067a955c5a31d
Fabian Indrunas <findrunas@protonmail.com>
2025-10-31 23:23:02 +0100

    WIP

--- flake.nix
+++ flake.nix
@@ -12,21 +12,56 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-	system = "x86_64-linux";
-	modules = [
-	    ./system/configuration.nix 
-	    home-manager.nixosModules.home-manager  
-	    {
-                  home-manager = {
+  outputs = { self, nixpkgs, home-manager, ... }: 
+    # Define helper function to build different kinds of systems
+    let 
+	makeSystemfunc = { modules }: nixpkgs.lib.nixosSystem {
+	   modules = modules ++ [
+	       ./system/configuration.nix 
+	       home-manager.nixosModules.home-manager  
+	       {
+                   home-manager = {
                        useGlobalPkgs = true; # what does  this do?
                        useUserPackages = true; # what does this do?
                        users.fabian = import ./home/home.nix;
                        backupFileExtension = "backup";
-		  };
-   	    }
-	];
-    };
+		   };
+   	       }
+	   ];
+        };
+    in
+  {
+      # Use makeSystemfunc helper function to built different nixos desktop experiences
+       nixosConfigurations = 
+       let 
+          # Define systems
+          qtile-ly-system = makeSystemfunc { modules = [ ./desktop/qtile-ly.nix ]; };
+          cosmic-system = makeSystemfunc { modules = [ ./desktop/cosmic.nix ]; };
+          hyperland-system = makeSystemfunc { modules = [ ./desktop/hyperland.nix ]; };
+       in 
+       {
+	  # Default system is nixos
+          nixos = qtile-ly-system; 
+          qtile-ly = qtile-ly-system;
+          cosmic = cosmic-system;
+          hyperland = hyperland-system;
+       };
+#
+    #nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+	#system = "x86_64-linux";
+	#modules = [
+	    #./system/configuration.nix 
+	    #home-manager.nixosModules.home-manager  
+	    #{
+                  #home-manager = {
+                       #useGlobalPkgs = true; # what does  this do?
+                       #useUserPackages = true; # what does this do?
+                       #users.fabian = import ./home/home.nix;
+                       #backupFileExtension = "backup";
+		  #};
+   	    #}
+	#];
+    #};
+
   };
 }
